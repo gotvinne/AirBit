336dcc42ac0f354c5818b350754a8b643728f187
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2025-07-20 19:07:25 +0200

    hand-controller: set steady state values

--- src/hand-controller/orientation.cpp
+++ src/hand-controller/orientation.cpp
@@ -7,6 +7,7 @@ int pitch = 0;
 int roll = 0;
 
 const int ANGULAR_THRESHOLD = 45;
+const int STEADY_STATE_THRESHOLD = 15;
 /*
  * Clamp the variable inside interval
  */
@@ -15,12 +16,23 @@ static int clampThreshold(int value, int threshold) {
                   threshold); // implementing std::clamp
 }
 
+static int clampAngles(int value, int threshold) {
+  int clampedValue = clampThreshold(value, threshold);
+
+  // Set steady state reference:
+  if (clampedValue > -STEADY_STATE_THRESHOLD &&
+      clampedValue < STEADY_STATE_THRESHOLD) {
+    return 0;
+  }
+  return clampedValue;
+}
+
 void setOrientation() {
   int measuredPitch = uBit.accelerometer.getPitch();
-  pitch = clampThreshold(measuredPitch, ANGULAR_THRESHOLD);
+  pitch = clampAngles(measuredPitch, ANGULAR_THRESHOLD);
 
   int measuredRoll = uBit.accelerometer.getRoll();
-  roll = clampThreshold(measuredRoll, ANGULAR_THRESHOLD);
+  roll = clampAngles(measuredRoll, ANGULAR_THRESHOLD);
 
   // uBit.display.scroll(pitch);
   // uBit.display.scroll(roll);
