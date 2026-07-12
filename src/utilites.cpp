2ce4ab4a62b71afb8f91aa93bd88ad0e0c1fe6a9
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2026-07-12 14:37:27 +0200

    utilities: Move the rest of the display logic

--- src/utilites.cpp
+++ src/utilites.cpp
@@ -3,6 +3,12 @@
 static int pulseDirection = 15;
 static int armedPulse = 0;
 
+static const int LED_ON = 255;
+
+static uint16_t angleToDisplayCoord(int angle) {
+  return static_cast<uint16_t>(std::round((angle + 45) / 22.5));
+}
+
 static void updatePulse() {
   armedPulse += pulseDirection;
 
@@ -20,4 +26,24 @@ void DisplayArmed(bool isArmed, Image &ledDisplay) {
   } else {
     armedPulse = 0;
   }
+}
+
+void DisplayThrottle(int throttle, Image &ledDisplay) {
+  if (throttle == 0) {
+    return;
+  } else {
+    int y = floor(4 - (throttle / 25));
+    ledDisplay.setPixelValue(0, y, LED_ON);
+  }
+}
+
+void DisplayPitchRoll(int roll, int pitch, Image &ledDisplay) {
+  uint16_t x = angleToDisplayCoord(roll);
+  uint16_t y = angleToDisplayCoord(pitch);
+  ledDisplay.setPixelValue(x, y, LED_ON);
+}
+
+void DisplayYaw(int yaw, Image &ledDisplay) {
+  uint16_t x = static_cast<uint16_t>(yaw / 30 + 2);
+  ledDisplay.setPixelValue(x, 0, LED_ON);
 }
