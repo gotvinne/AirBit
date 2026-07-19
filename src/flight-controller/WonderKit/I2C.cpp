b2733157ebb73054ef31048aa99ab3bb2738dd3f
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2026-07-19 13:02:24 +0200

    flight-controller: Make wonderkit folder
    Update servoController.cpp

---
+++ src/flight-controller/WonderKit/I2C.cpp
@@ -0,0 +1,13 @@
+#include "I2C.h"
+#include "../flightController.h"
+
+MicroBitPin SDA = uBit.io.P2;
+MicroBitPin SCL = uBit.io.P1;
+
+void InitI2C() {
+  // Redirect I2C to use P1 and P2.
+  if (uBit.i2c.redirect(uBit.io.P2, uBit.io.P1)) {
+    SetState(State::PANIC);
+    uBit.display.print("I2C");
+  }
+}
---
+++ src/flight-controller/WonderKit/servoController.cpp
@@ -0,0 +1,81 @@
+#include "servoController.h"
+#include "../flightController.h"
+#include "../utilities.h"
+#include "I2C.h"
+#include <MicroBit.h>
+
+const uint16_t SERVO_CONTROLLER_ADDR = 196; // 8-bit SLAVE ADDRESS
+
+// Servo controller registers
+const uint8_t SERVO_CONTROLLER_MODE1_REG = 0;
+const uint8_t SERVO_CONTROLLER_MODE2_REG = 1;
+const uint8_t SERVO_CONTROLLER_MODE3_REG = 8;
+// Servo controller register values
+// https://github.com/gomakekit/Airbit_V2/blob/master/custom.ts#L459
+const uint8_t SERVO_CONTROLLER_MODE1_VALUE = 128;
+const uint8_t SERVO_CONTROLLER_MODE2_VALUE = 5;
+const uint8_t SERVO_CONTROLLER_MODE3_VALUE = 170;
+
+// Servo Motors
+const int NUM_MOTORS = 4;
+const uint8_t MOTOR_CW1_REG = 4;
+const uint8_t MOTOR_CCW1_REG = 2;
+const uint8_t MOTOR_CW2_REG = 3;
+const uint8_t MOTOR_CCW2_REG = 5;
+const int MAX_ACTUATION = 255;
+const int MIN_ACTUATION = 0;
+
+static void failSafe() {
+  SetState(State::PANIC);
+  uBit.display.print("NM"); // No Motor detected
+}
+
+static void WriteToServoController(uint8_t reg, uint8_t value) {
+  if (uBit.i2c.writeRegister(SERVO_CONTROLLER_ADDR, reg, value)) {
+    failSafe();
+  }
+}
+
+static void ReadServoController() {
+  uint8_t buf[2] = {0, 0};
+  if (uBit.i2c.read(SERVO_CONTROLLER_ADDR, buf, 2)) {
+    failSafe();
+  } else {
+    uBit.display.print("M"); // Motor detected
+    uBit.sleep(3000);
+  }
+}
+
+static void SetPropellerActuation(uint8_t motor, uint8_t actuation) {
+  actuation = Clamp(actuation, MIN_ACTUATION, MAX_ACTUATION);
+  if (uBit.i2c.writeRegister(SERVO_CONTROLLER_ADDR, motor, actuation)) {
+    failSafe();
+    return;
+  }
+
+  uBit.sleep(10);
+}
+
+void SetAllPropellerActuation(uint8_t cw1_v, uint8_t cw2_v, uint8_t ccw1_v,
+                              uint8_t ccw2_v) {
+  const uint8_t motorRegs[] = {MOTOR_CW1_REG, MOTOR_CW2_REG, MOTOR_CCW1_REG,
+                               MOTOR_CCW2_REG};
+  const uint8_t motorValues[] = {cw1_v, cw2_v, ccw1_v, ccw2_v};
+
+  for (int i = 0; i < NUM_MOTORS; ++i) {
+    SetPropellerActuation(motorRegs[i], motorValues[i]);
+  }
+}
+
+void InitServoController() {
+  // Configure the servo controller with default values.
+  WriteToServoController(SERVO_CONTROLLER_MODE1_REG,
+                         SERVO_CONTROLLER_MODE1_VALUE);
+  WriteToServoController(SERVO_CONTROLLER_MODE2_REG,
+                         SERVO_CONTROLLER_MODE2_VALUE);
+  WriteToServoController(SERVO_CONTROLLER_MODE3_REG,
+                         SERVO_CONTROLLER_MODE3_VALUE);
+  SetAllPropellerActuation(0, 0, 0, 0);
+  // Detect WonderKit powered.
+  ReadServoController();
+}
