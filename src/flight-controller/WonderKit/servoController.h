126ec39df8e18595aa8415dc7a5c6dcf8b42811d
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2026-07-19 22:53:58 +0200

    flight-controller: set actuation based on state

--- src/flight-controller/WonderKit/servoController.h
+++ src/flight-controller/WonderKit/servoController.h
@@ -3,5 +3,4 @@
 
 void InitServoController();
 
-void SetAllPropellerActuation(uint8_t cw1_v, uint8_t cw2_v, uint8_t ccw1_v,
-                              uint8_t ccw2_v);
+void SetPropellerActuation();
