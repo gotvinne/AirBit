fe04c0dc3fc490a5e7eba6d49e6eff9936d98c64
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2026-08-14 23:50:02 +0200

    flight-controller: rename blackboard

--- src/flight-controller/BlackBoard/servo.cpp
+++ src/flight-controller/BlackBoard/servo.cpp
@@ -1,28 +1,28 @@
-#include "servoController.h"
+#include "servo.h"
 #include "../flightController.h"
 #include "../utilities.h"
 #include "I2C.h"
 #include <MicroBit.h>
 
-const uint16_t SERVO_CONTROLLER_ADDR = 196; // 8-bit SLAVE ADDRESS
+constexpr uint16_t SERVO_ADDR = 196; // 0xC4, (98 << 1) 8-bit SLAVE ADDRESS
 
 // Servo controller registers
-const uint8_t SERVO_CONTROLLER_MODE1_REG = 0;
-const uint8_t SERVO_CONTROLLER_MODE2_REG = 1;
-const uint8_t SERVO_CONTROLLER_MODE3_REG = 8;
+constexpr uint8_t SERVO_MODE1_REG = 0;
+constexpr uint8_t SERVO_MODE2_REG = 1;
+constexpr uint8_t SERVO_MODE3_REG = 8;
 // Servo controller register values
 // https://github.com/gomakekit/Airbit_V2/blob/master/custom.ts#L459
-const uint8_t SERVO_CONTROLLER_MODE1_VALUE = 128;
-const uint8_t SERVO_CONTROLLER_MODE2_VALUE = 5;
-const uint8_t SERVO_CONTROLLER_MODE3_VALUE = 170;
+constexpr uint8_t SERVO_MODE1_VALUE = 128;
+constexpr uint8_t SERVO_MODE2_VALUE = 5;
+constexpr uint8_t SERVO_MODE3_VALUE = 170;
 
 // Servo Motors
-const uint8_t MOTOR_CW1_REG = 4;
-const uint8_t MOTOR_CCW1_REG = 2;
-const uint8_t MOTOR_CW2_REG = 3;
-const uint8_t MOTOR_CCW2_REG = 5;
-const uint8_t motorRegs[] = {MOTOR_CW1_REG, MOTOR_CW2_REG, MOTOR_CCW1_REG,
-                             MOTOR_CCW2_REG};
+constexpr uint8_t MOTOR_CW1_REG = 4;
+constexpr uint8_t MOTOR_CCW1_REG = 2;
+constexpr uint8_t MOTOR_CW2_REG = 3;
+constexpr uint8_t MOTOR_CCW2_REG = 5;
+constexpr uint8_t motorRegs[] = {MOTOR_CW1_REG, MOTOR_CW2_REG, MOTOR_CCW1_REG,
+                                 MOTOR_CCW2_REG};
 
 const int MAX_ACTUATION = 255;
 const int MIN_ACTUATION = 0;
@@ -35,14 +35,14 @@ static void failSafe() {
 }
 
 static void WriteToServoController(uint8_t reg, uint8_t value) {
-  if (uBit.i2c.writeRegister(SERVO_CONTROLLER_ADDR, reg, value)) {
+  if (uBit.i2c.writeRegister(SERVO_ADDR, reg, value)) {
     failSafe();
   }
 }
 
 static void readServoController() {
   uint8_t buf[2] = {0, 0};
-  if (uBit.i2c.read(SERVO_CONTROLLER_ADDR, buf, 2)) {
+  if (uBit.i2c.read(SERVO_ADDR, buf, 2)) {
     failSafe();
   } else {
     uBit.display.print("M"); // Motor detected
@@ -52,7 +52,7 @@ static void readServoController() {
 
 static void WritePropellerActuation(uint8_t motor, uint8_t actuation) {
   actuation = Clamp(actuation, MIN_ACTUATION, MAX_ACTUATION);
-  if (uBit.i2c.writeRegister(SERVO_CONTROLLER_ADDR, motor, actuation)) {
+  if (uBit.i2c.writeRegister(SERVO_ADDR, motor, actuation)) {
     failSafe();
     return;
   }
@@ -72,16 +72,13 @@ void SetPropellerActuation() {
   }
 }
 
-void InitServoController() {
+void InitServo() {
   // Configure the servo controller with default values.
-  WriteToServoController(SERVO_CONTROLLER_MODE1_REG,
-                         SERVO_CONTROLLER_MODE1_VALUE);
-  WriteToServoController(SERVO_CONTROLLER_MODE2_REG,
-                         SERVO_CONTROLLER_MODE2_VALUE);
-  WriteToServoController(SERVO_CONTROLLER_MODE3_REG,
-                         SERVO_CONTROLLER_MODE3_VALUE);
+  WriteToServoController(SERVO_MODE1_REG, SERVO_MODE1_VALUE);
+  WriteToServoController(SERVO_MODE2_REG, SERVO_MODE2_VALUE);
+  WriteToServoController(SERVO_MODE3_REG, SERVO_MODE3_VALUE);
   SetPropellerActuation(); // Set all motors to 0 actuation due to initial
                            // state.
-  // Detect WonderKit powered.
+  // Detect BlackBoard powered.
   readServoController();
 }
--- src/flight-controller/BlackBoard/servo.h
+++ src/flight-controller/BlackBoard/servo.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <cstdint>
 
-void InitServoController();
+void InitServo();
 
 void SetPropellerActuation();
