52df73adeb2b9318e2dcb7c0f5f864693a97288f
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2026-08-16 15:44:29 +0200

    hand-controller: add calibration routine to roll and pitch
    - add a steady threshold to 10 degrees

--- src/hand-controller/orientation.cpp
+++ src/hand-controller/orientation.cpp
@@ -6,30 +6,50 @@
 // orientation variables:
 static int pitch = 0;
 static int roll = 0;
+static int pitchOffset = 0;
+static int rollOffset = 0;
 
-const int ANGULAR_THRESHOLD = 45;
-const int STEADY_STATE_THRESHOLD = 15;
+constexpr int CALIBRATION_SAMPLES = 100;
+constexpr int ANGULAR_THRESHOLD = 45;
+constexpr int STEADY_STATE_THRESHOLD = 10;
 
 static int clampAngles(int value, int threshold) {
   int clampedValue = Clamp(value, -threshold, threshold);
 
   // Set steady state reference:
-  if (clampedValue > -STEADY_STATE_THRESHOLD &&
-      clampedValue < STEADY_STATE_THRESHOLD) {
+  if (clampedValue > STEADY_STATE_THRESHOLD) {
+    return clampedValue - STEADY_STATE_THRESHOLD;
+  } else if (clampedValue < -STEADY_STATE_THRESHOLD) {
+    return clampedValue + STEADY_STATE_THRESHOLD;
+  } else {
     return 0;
   }
   return clampedValue;
 }
 
+void CalibrateHandAccelerometer() {
+  int rollSum = 0;
+  int pitchSum = 0;
+
+  for (int i = 0; i <= CALIBRATION_SAMPLES; i++) {
+    rollSum += uBit.accelerometer.getRoll();
+    pitchSum += uBit.accelerometer.getPitch();
+    uBit.sleep(5);
+  }
+
+  rollOffset = rollSum / CALIBRATION_SAMPLES;
+  pitchOffset = pitchSum / CALIBRATION_SAMPLES;
+}
+
 void SetOrientation() {
-  int measuredPitch = uBit.accelerometer.getPitch();
+  int measuredPitch = uBit.accelerometer.getPitch() - pitchOffset;
   pitch = clampAngles(measuredPitch, ANGULAR_THRESHOLD);
 
-  int measuredRoll = uBit.accelerometer.getRoll();
+  int measuredRoll = uBit.accelerometer.getRoll() - rollOffset;
   roll = clampAngles(measuredRoll, ANGULAR_THRESHOLD);
 
-  // uBit.display.scroll(pitch);
-  // uBit.display.scroll(roll);
+  // uBit.serial.printf("roll: %d, pitch: %d\n rollOffset: %d, pitchOffset:
+  // %d\n", roll, pitch, rollOffset, pitchOffset);
 }
 
 int GetPitch() { return pitch; }
--- src/hand-controller/orientation.h
+++ src/hand-controller/orientation.h
@@ -2,5 +2,6 @@
 #include <algorithm>
 
 void SetOrientation();
+void CalibrateHandAccelerometer();
 int GetPitch();
 int GetRoll();
